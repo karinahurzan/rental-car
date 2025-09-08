9066ef66f754aeb38defac3fcb2bd050a71093d1
karinahurzan <kgurzan@gmail.com>
2025-09-08 22:18:30 +0300

    correct imports

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import { Link, NavLink } from "react-router-dom";
-import Icon from "../../assets/Icon";
 import css from "./Header.module.css";
+import Icon from "../../../public/assets/Icon";
 
 export default function Header() {
   return (
